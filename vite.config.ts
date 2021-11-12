057075a074b2567b8ff1cda3da3ebaa073ff5f3a
djhi <1122076+djhi@users.noreply.github.com>
2021-11-12 10:01:15 +0100

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -17,72 +17,76 @@ const sourceMapsInProduction = false;
 /**********                                              Vite                                               **********/
 /*********************************************************************************************************************/
 
-import { defineConfig, UserConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import path from 'path';
-import sveltePreprocess from 'svelte-preprocess';
-import legacy from '@vitejs/plugin-legacy';
-import autoprefixer from 'autoprefixer';
-import pkg from './package.json';
-import tsconfig from './tsconfig.json';
+import { defineConfig, UserConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
+import sveltePreprocess from "svelte-preprocess";
+import { optimizeImports, optimizeCss } from "carbon-preprocess-svelte";
+import legacy from "@vitejs/plugin-legacy";
+import autoprefixer from "autoprefixer";
+import pkg from "./package.json";
+import tsconfig from "./tsconfig.json";
 
-const production = process.env.NODE_ENV === 'production';
-const config = <UserConfig> defineConfig({
-	plugins: [
-		svelte({
-			emitCss: production,
-			preprocess: sveltePreprocess(),
-			compilerOptions: {
-				dev: !production,
-			},
+const production = process.env.NODE_ENV === "production";
+const config = <UserConfig>defineConfig({
+  plugins: [
+    svelte({
+      emitCss: production,
+      preprocess: [sveltePreprocess(), optimizeImports()],
+      compilerOptions: {
+        dev: !production,
+      },
 
-			// @ts-ignore This is temporary until the type definitions are fixed!
-			hot: !production
-		}),
-	],
-	server: {
-		host: 'localhost',
-		port: 5000
-	},
-	build: {
-		sourcemap: sourceMapsInProduction
-	},
-	css: {
-		postcss: {
-			plugins: [
-				autoprefixer()
-			]
-		}
-	}
+      // @ts-ignore This is temporary until the type definitions are fixed!
+      hot: !production,
+    }),
+    process.env.NODE_ENV === "production" && optimizeCss(),
+  ],
+  server: {
+    host: "localhost",
+    port: 5000,
+  },
+  build: {
+    sourcemap: sourceMapsInProduction,
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
 });
 
 // Babel
 if (useBabel) {
-	config.plugins.unshift(
-		legacy({
-			targets: pkg.browserslist
-		})
-	);
+  config.plugins.unshift(
+    legacy({
+      targets: pkg.browserslist,
+    })
+  );
 }
 
 // Load path aliases from the tsconfig.json file
 const aliases = tsconfig.compilerOptions.paths;
 
 for (const alias in aliases) {
-	const paths = aliases[alias].map((p: string) => path.resolve(__dirname, p));
+  const paths = aliases[alias].map((p: string) => path.resolve(__dirname, p));
 
-	// Our tsconfig uses glob path formats, whereas webpack just wants directories
-	// We'll need to transform the glob format into a format acceptable to webpack
+  // Our tsconfig uses glob path formats, whereas webpack just wants directories
+  // We'll need to transform the glob format into a format acceptable to webpack
 
-	const wpAlias = alias.replace(/(\\|\/)\*$/, '');
-	const wpPaths = paths.map((p: string) => p.replace(/(\\|\/)\*$/, ''));
+  const wpAlias = alias.replace(/(\\|\/)\*$/, "");
+  const wpPaths = paths.map((p: string) => p.replace(/(\\|\/)\*$/, ""));
 
-	if (!config.resolve) config.resolve = {};
-	if (!config.resolve.alias) config.resolve.alias = {};
+  if (!config.resolve) config.resolve = {};
+  if (!config.resolve.alias) config.resolve.alias = {};
 
-	if (config.resolve && config.resolve.alias && !(wpAlias in config.resolve.alias)) {
-		config.resolve.alias[wpAlias] = wpPaths.length > 1 ? wpPaths : wpPaths[0];
-	}
+  if (
+    config.resolve &&
+    config.resolve.alias &&
+    !(wpAlias in config.resolve.alias)
+  ) {
+    config.resolve.alias[wpAlias] = wpPaths.length > 1 ? wpPaths : wpPaths[0];
+  }
 }
 
 export default config;
